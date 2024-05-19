e04e1e382118f6174734b991caaa6e748da4f15a
Caricasulo Francesco <caricasulo.francesco@educ.di.unito.it>
2024-05-19 14:45:32 +0000

    Delete compar.h

--- Esercizio 1/main/misc/compar.h
+++
@@ -1,5 +0,0 @@
-int compar_int(const void *a, const void *b);
-int compar_double(const void *a, const void *b);
-int compar_char(const void *a, const void *b);
-int compar_string(const void *a, const void *b);
-int compar_int_data(const void *a, const void *b);
